be48a03d879a596a21383b693d102f4342148a5d
333G <sgndjk@outlook.com>
2018-01-05 17:29:27 +0800

    Add .net core solution
    试试用.net core

---
+++ dotNetcore_for_selfweb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dotNetcore_for_selfweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
